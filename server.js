fd8171464d7fa4aa76be3b992212d7cb51ca8be4
Melvin Peepers <peepersm@gmail.com>
2019-08-14 00:54:38 -0400

    added custom logger on server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = require("express");
+
+// const postRouter = require("./posts/postRouter.js");
+// const userRouter = require("./users/userRouter.js");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+// middleware - converts the data to JSON
+server.use(express.json());
+
+// server.use("/api/posts", postRouter);
+// server.use("/api/users", userRouter);
+
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  const currentTime = new Date().toISOString();
+  console.log(
+    `Request: ${req.method}, Request URL: ${
+      req.url
+    }, Timestamp: ${currentTime} `
+  );
+}
 
 module.exports = server;
